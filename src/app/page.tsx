cce2720d611c32df87a217d9a158815e6af5b8fe
José Antonio García Solano <joseagarsol@gmail.com>
2026-02-23 19:54:27 +0100

    feat: Introduce a responsive sidebar layout, new UI components, and integrate them into the main page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import prisma from "@/lib/prisma";
 import TasksPageClient from "@/features/tasks/components/TasksPageClient";
 import { Task } from "@/features/tasks/types";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import MainSidebar from "@/components/layout/MainSidebar";
+import { Separator } from "@/components/ui/separator";
 
 export default async function Home() {
   const tasks = await prisma.task.findMany({
@@ -17,5 +24,18 @@ export default async function Home() {
         : (task.status as Task["status"]),
   }));
 
-  return <TasksPageClient initialTasks={formattedTasks} />;
+  return (
+    <SidebarProvider>
+      <MainSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <TasksPageClient initialTasks={formattedTasks} />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
