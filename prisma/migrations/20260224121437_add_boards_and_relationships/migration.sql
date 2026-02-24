f01eb25e2a5f5ff20a3853a3b4d2c1f40b6eae14
José Antonio García Solano <joseagarsol@gmail.com>
2026-02-24 13:21:40 +0100

    feat: implement board and board member models, linking tasks to boards and users, and updating the seed script.

---
+++ prisma/migrations/20260224121437_add_boards_and_relationships/migration.sql
@@ -0,0 +1,48 @@
+/*
+  Warnings:
+
+  - Added the required column `authorId` to the `Task` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `boardId` to the `Task` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('Owner', 'Editor', 'Viewer');
+
+-- AlterTable
+ALTER TABLE "Task" ADD COLUMN     "assigneeId" TEXT,
+ADD COLUMN     "authorId" TEXT NOT NULL,
+ADD COLUMN     "boardId" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Board" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Board_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BoardMember" (
+    "userId" TEXT NOT NULL,
+    "boardId" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+
+    CONSTRAINT "BoardMember_pkey" PRIMARY KEY ("userId","boardId")
+);
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_assigneeId_fkey" FOREIGN KEY ("assigneeId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BoardMember" ADD CONSTRAINT "BoardMember_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BoardMember" ADD CONSTRAINT "BoardMember_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
